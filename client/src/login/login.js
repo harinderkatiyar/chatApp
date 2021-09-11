0316fa630e99ea52f272e62363545a683d23cc64
Anees J <anees.js@outlook.com>
2021-09-11 13:41:11 +0100

    Initial Commit on main

---
+++ client/src/login/login.js
@@ -0,0 +1,56 @@
+$(document).ready(function () {
+	/**
+LOGIN VALUE
+	**/     console.log($.BASE_URL, "hello login");
+ const loginUserName = document.getElementById("loginUserName");
+ const loginPassword = document.getElementById("loginPassword");
+
+ /**
+SAVE LOGIN DATA AND CHECK SIGNUP DATA
+**/
+ saveLoginData = function (e) {
+
+  let LoginData = {
+   username: loginUserName.value,
+   password: loginPassword.value
+  }
+  console.log($.BASE_URL, "hello login");
+  console.log("=====testing==", LoginData);
+
+  return false
+  $.ajax({
+   url: $.BASE_URL + "api/register",
+   type: 'post',
+   data: JSON.stringify(loginData),
+   dataType: "json",
+   headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+   },
+   async: true,
+   contentType: false,
+   processData: false,
+   cache: false,
+   success: function (data, status) {
+    console.log("-----", data);
+    // console.info("check-login", data);
+    // if (data.status == true) {
+    // 	console.warn("Registration has been completed");
+    // 	toastr.success('You have been successfully registered.')
+    // 	loginUserName.value = "";
+    // 	loginPassword.value = "";
+    // 	setTimeout(function () {
+    // 		$('#login').trigger('click');
+    // 	}, 1000);
+    // } else {
+    // 	toastr.error('Something went wrong!', 'Error');
+    // }
+    // return false; // Prevent page refresh
+   },
+   // 	// error: function (err) {
+   // 	// 	toastr.error('Something went wrong!', 'Error');
+   // 	// }
+  });
+  return false;
+ }
+});
--- server/routes/index.js
+++ server/routes/index.js
@@ -4,5 +4,5 @@ const controller = require("../controller/index");
 
 // Hr/admin 
 router.post('/register', controller.register);
-
+router.post('/login', controller.login);
 module.exports = router;
